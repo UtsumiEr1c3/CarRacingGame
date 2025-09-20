0a07aaaccc5354454c704964a267acdae1f6e684
UtsumiEr1c3 <glhf2233@163.com>
2025-09-20 21:23:25 +0800

    add: Perlin noise test

---
+++ Assets/Scripts/TestPerlin.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class TestPerlin : MonoBehaviour
+{
+    private LineRenderer lineRenderer;
+    private float a = 0.06f; // 间隙, 平滑度
+    public bool isUsePerLin;
+
+    private void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        Vector3[] posArr = new Vector3[100];
+        float ranX = Random.Range(1, 1000);
+        float ranY = Random.Range(1, 1000);
+        for (int i = 0; i < posArr.Length; i++)
+        {
+            if (isUsePerLin)
+            {
+                posArr[i] = new Vector3(i * 0.1f, Mathf.PerlinNoise(i * a + ranX, i * a + ranY), 0);
+            }
+            else
+            {
+                posArr[i] = new Vector3(i * 0.1f, Random.value, 0);
+            }
+        }
+        lineRenderer.SetPositions(posArr);
+    }
+}
